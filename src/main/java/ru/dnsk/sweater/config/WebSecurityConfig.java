60b37d4c7323dbef66a2aecff2b88ae474fca88d
Сергей <serzhprostomolodets@mail.ru>
2021-09-10 15:51:29 +0300

    Add front

--- src/main/java/ru/dnsk/sweater/config/WebSecurityConfig.java
+++ src/main/java/ru/dnsk/sweater/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration", "/static").permitAll()
+                .antMatchers("/", "/registration", "/static/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/ru/dnsk/sweater/model/User.java
+++ src/main/java/ru/dnsk/sweater/model/User.java
@@ -35,6 +35,10 @@ public class User implements UserDetails {
         this.roles = roles;
     }
 
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
+
     public int getId() {
         return id;
     }
